39fbc1e71f3827fb6edfef1b3e6fb42cdb8dd9a3
Shuhrat <shuhratodilbekov3@gmail.com>
2026-04-25 17:51:23 +0500

    Use Laravel default web middleware stack to fix session store errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (! $this->app->isProduction()) {
+            return;
+        }
+
+        if (config('session.driver') === 'database') {
+            config(['session.driver' => 'file']);
+        }
+
+        if (config('cache.default') === 'database') {
+            config(['cache.default' => 'file']);
+        }
+
+        if (config('queue.default') === 'database') {
+            config(['queue.default' => 'sync']);
+        }
     }
 }
